d45370d0be060e7d47b7a43d0f96dccdec7a0ef3
kedargodhani <kedargodhani22@gmail.com>
2024-03-28 11:03:40 +0530

    jj

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(slider1).ready(function (){
+    $("#slider1").owlCarousel({
+        items:1,
+        nav:true,
+        margin:30,
+        dosts:true,
+        autoplay:true,
+        autoplaytimeout:1000,
+    
+    });
+    $("#testimonial").owlCarousel({
+        items:3,
+        nav:true,
+        margin:30,
+        dosts:true,
+    
+    });
+    
+})
